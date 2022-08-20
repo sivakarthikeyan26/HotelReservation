4d10e120f7774567bba163d1c2f4061eb488d553
Your Name <you@example.com>
2022-08-20 19:58:59 +0530

    program to add the hotel name and rates is implemented

---
+++ src/workshop3/Hotel.java
@@ -0,0 +1,17 @@
+package workshop3;
+
+public class Hotel {
+	private String name;
+	private int rates;
+	
+	/**
+	 * Constructor for storing the hotel name and rates
+	 * @param name
+	 * @param rates
+	 */
+	public Hotel(String name, int rate) {
+		this.name=name;
+		this.rates=rate;
+		
+	}
+}
