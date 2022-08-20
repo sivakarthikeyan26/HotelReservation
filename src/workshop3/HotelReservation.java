8cdd297853363e39f86ff58cd0de054a2e428059
Your Name <you@example.com>
2022-08-20 19:42:11 +0530

    Program to print hotel reservation system

---
+++ src/workshop3/HotelReservation.java
@@ -0,0 +1,13 @@
+package workshop3;
+
+public class HotelReservation {
+	/**
+	 * Main method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to hotel Reservation");
+
+	}
+
+}
